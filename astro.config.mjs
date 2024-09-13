3cda1e1327c6edd76ea91f415a62dffe68f66082
Mohamed A. Salah <me@msalah.net>
2024-09-13 19:36:11 +0300

    change the base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   // change this to your own domain
-  site: "https://msalah.net/barebones/",
+  site: "https://msalah.net",
+  base: "/barebones",
   integrations: [tailwind(), sitemap()],
 });
